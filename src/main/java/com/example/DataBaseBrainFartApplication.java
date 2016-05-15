b4f2e31a8305cb9afc898134c9b0e3e9a6e1219a
mystic <harutyunyan15vip@gmail.com>
2016-05-15 10:51:51 +0300

    Spring

---
+++ src/main/java/com/example/DataBaseBrainFartApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseBrainFartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseBrainFartApplication.class, args);
+	}
+}
