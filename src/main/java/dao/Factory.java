9e3843e70442aedcdb650aad6ff3874478338a7a
Gayane <harutyunyan15vip@gmail.com>
2016-06-02 06:38:29 +0300

    hibernate,factory

--- src/main/java/dao/Factory.java
+++ src/main/java/dao/Factory.java
@@ -1,11 +1,21 @@
 package dao;
 
-import dao.postgres.PostgreSqlUsersDao;
+import dao.postgres.*;
 
 
 public class Factory {
     public static Factory factory = new Factory();
-    public UsersDao usersDao;
+    private UsersDao usersDao;
+    private TestSessionDao testSessionDao;
+    private TestsDao testsDao;
+    private TestAvailabilityDao testAvailabilityDao;
+    private SubjectsDao subjectsDao;
+    private QuestionResultsDao questionResultsDao;
+    private QuestionDao questionDao;
+    private QuestionConnectionsDao questionConnectionsDao;
+    private AnswerOptionsDao answerOptionsDao;
+    private AdminDao adminDao;
+
 
     private Factory() {
 
@@ -18,5 +28,50 @@ public class Factory {
     public UsersDao getUsersDao() {
         if (usersDao == null) usersDao = new PostgreSqlUsersDao();
         return usersDao;
+    }
+
+    public TestSessionDao getTestSessionDao() {
+        if (testSessionDao == null) testSessionDao = new PostgresSqlTestSessionDao();
+         return testSessionDao;
+    }
+
+    public TestsDao getTestsDao() {
+        if (testsDao == null) testsDao = new PostgreSqlTestsDao();
+        return testsDao;
+    }
+
+    public TestAvailabilityDao getTestAvailabilityDao() {
+        if (testAvailabilityDao == null) testAvailabilityDao = new PostgreSqlTestAvailabilityDao();
+        return testAvailabilityDao;
+    }
+
+    public SubjectsDao getSubjectsDao() {
+        if (subjectsDao == null) subjectsDao = new PostgreSqlSubjectsDao();
+        return subjectsDao;
+    }
+
+    public QuestionResultsDao getQuestionResultsDao() {
+        if (questionResultsDao == null) questionResultsDao = new PostgreSqlQuestionResultsDao();
+        return questionResultsDao;
+    }
+
+    public QuestionDao getQuestionDao() {
+        if (questionDao == null) questionDao = new PostgreSqlQuestionDao();
+        return questionDao;
+    }
+
+    public AnswerOptionsDao getAnswerOptionsDao() {
+        if (answerOptionsDao == null) answerOptionsDao = new PostgreSqlAnswerOptionsDao();
+        return answerOptionsDao;
+    }
+
+    public AdminDao getAdminDao() {
+        if (adminDao == null) adminDao = new PosgreSqlAdminDao();
+        return adminDao;
+    }
+
+    public QuestionConnectionsDao getQuestionConnectionsDao() {
+        if (questionConnectionsDao == null) questionConnectionsDao = new PostgreSqlQuestionConnectionsDao();
+        return questionConnectionsDao;
     }
 }
--- src/main/java/dao/Project.java
+++ src/main/java/dao/Project.java
@@ -1,9 +1,8 @@
 package dao;
 
+import model.*;
 
-import model.Users;
-
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by gaya on 02.06.2016.
@@ -12,6 +11,18 @@ public class Project {
     public static void main(String[] args) {
         Factory factory = Factory.getFactory();
         UsersDao usersDao = factory.getUsersDao();
+        TestSessionDao testSessionDao = factory.getTestSessionDao();
+        TestsDao testsDao = factory.getTestsDao();
+        TestAvailabilityDao testAvailabilityDao = factory.getTestAvailabilityDao();
+        SubjectsDao subjectsDao = factory.getSubjectsDao();
+        QuestionResultsDao questionResultsDao = factory.getQuestionResultsDao();
+        QuestionDao questionDao = factory.getQuestionDao();
+        QuestionConnectionsDao questionConnectionsDao = factory.getQuestionConnectionsDao();
+        AnswerOptionsDao answerOptionsDao = factory.getAnswerOptionsDao();
+        AdminDao adminDao = factory.getAdminDao();
+
+
+
 /*
         Users users = new Users();
         users.setEmail("harutyunyan15@mail.ru");
