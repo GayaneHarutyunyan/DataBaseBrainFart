e024c98735168bb67a65c2b22df3e799dce81fd1
Gayane <harutyunyan15vip@gmail.com>
2016-07-18 19:38:53 +0300

    Update AlgorithmTest.java

--- src/main/java/com/brainfart/AlgorithmTest.java
+++ src/main/java/com/brainfart/AlgorithmTest.java
@@ -15,7 +15,7 @@ public class AlgorithmTest {
         String str = scanner.next();
 
         int lengthArray = str.length();
-        boolean flag = true;
+        boolean fg = true;
         Stack<String> stack = new Stack<>();
         for (int i = 0; i < lengthArray; i++) {
             String algorithmTest = str.substring(i, i + 1);
@@ -27,40 +27,40 @@ public class AlgorithmTest {
                 stack.push(algorithmTest);
             } else if (algorithmTest.equals(")")) {
                 if (stack.empty()) {
-                    flag = false;
+                    fg = false;
                     break;
                 }
                 if (stack.peek().equals("(")) {
                     stack.pop();
                 } else {
-                    flag = false;
+                    fg = false;
                     break;
                 }
             } else if (algorithmTest.equals("}")) {
                 if (stack.empty()) {
-                    flag = false;
+                    fg = false;
                     break;
                 }
                 if (stack.peek().equals("{")) {
                     stack.pop();
                 } else {
-                    flag = false;
+                    fg = false;
                     break;
                 }
             } else if (algorithmTest.equals("]")) {
                 if (stack.empty()) {
-                    flag = false;
+                    fg = false;
                     break;
                 }
                 if (stack.peek().equals("[")) {
                     stack.pop();
                 } else {
-                    flag = false;
+                    fg = false;
                     break;
                 }
             }
         }
-        if (flag && stack.empty())
+        if (fg && stack.empty())
             System.out.println("true");
         else
             System.out.println("false");
